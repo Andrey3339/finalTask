89974cdc8d7d241f3f4c901c0130fb43a08858d0
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-03-12 12:38:30 +0300

    added task code

---
+++ finalTask/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите последовательность строк, разделяя их пробелом: ");
+string[] sourceArray = Console.ReadLine().Split(' ');
+string[] targetArray = new string[sourceArray.Length];
+int t = 0;
+for (int i = 0; i < sourceArray.Length; i++)
+{
+    if (sourceArray[i].Length <= 3)
+    {
+        targetArray[t] = sourceArray[i];
+        t++;
+    }
+}
+Array.Resize(ref targetArray, t);
+Console.WriteLine("Полученный массив: ");
+Console.WriteLine(string.Join(", ", targetArray));
+
+
+
+
